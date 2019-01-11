a294ebe128f5fa3c754e57503b5c94feeda5a9a9
Oleg <oleg.dubovitsky@websailors.pro>
2019-01-11 17:46:07 +0300

    update v1.3

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
   $(".js--section-features").waypoint(
     function(direction) {
       if (direction == "down") {
-        $("nav").addClass("sticky");
+        {
+          $("nav").addClass("sticky");
+        }
       } else {
         $("nav").removeClass("sticky");
       }
@@ -70,4 +72,55 @@ $(document).ready(function() {
         }
       }
     });
+
+  // animations on scroll
+
+  $(".js--wp-1").waypoint(
+    function(direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%"
+    }
+  );
+  $(".js--wp-2").waypoint(
+    function(direction) {
+      $(".js--wp-2").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%"
+    }
+  );
+  $(".js--wp-3").waypoint(
+    function(direction) {
+      $(".js--wp-3").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%"
+    }
+  );
+
+  $(".js--wp-4").waypoint(
+    function(direction) {
+      $(".js--wp-4").addClass("animated pulse");
+    },
+    {
+      offset: "50%"
+    }
+  );
+
+  // Mobile nav
+
+  $(".js--nav-icon").click(function() {
+    let nav = $(".js--main-nav");
+    let icon = $(".js--nav-icon i");
+    nav.slideToggle(200);
+    if (icon.hasClass("ion-navicon-round")) {
+      icon.addClass("ion-close-round");
+      icon.removeClass("ion-navicon-round");
+    } else {
+      icon.addClass("ion-navicon-round");
+      icon.removeClass("ion-close-round");
+    }
+  });
 });
